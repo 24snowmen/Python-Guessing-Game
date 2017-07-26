68b5eb6930d10c81c8ad83cf0986d921ccfebb48
24snowmen <30446046+24snowmen@users.noreply.github.com>
2017-07-26 14:53:30 -0500

    led and buzzer

--- python3.py
+++ python3.py
@@ -1,31 +1,60 @@
 #!/usr/bin/env python
 
+import RPi.GPIO as GPIO
+import time
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+led=11
+buz=32
+GPIO.setup(led, GPIO.OUT)
+GPIO.setup(buz, GPIO.OUT)
+buzz = GPIO.PWM(buz, 110)
 import random
 number = random.randint(1,100)
+
+def buzzn:
+    buzz.start(15)
+    time.sleep(.5)
+    buzz.stop()
+
 play = True
 guess = True
 while play:
     while guess:
         a = int(raw_input("Guess a whole number between 1 and 100:"))
         if a == number:
+            GPIO.output(led, True)
+            time.sleep(5)
+            GPIO.output(led, False)
             print "You guessed it, the number was", number
             guess = False
             print "You win"
         elif a > number:
-                if a - number > 15:
-                    print a, "is very high"
-                else:
-                    print a, "is too high"
+            if a - number > 15:
+                print a, "is very high"
+                buzzn
+                time.sleep(1)
+                buzzn
+            else:
+                print a, "is too high"
+                buzz.start(15)
+                time.sleep(.5)
+                buzz.stop()
         elif a < number:
             if number - a > 15:
                 print a, "is very low"
+                buzzn
+                time.sleep(1)
+                buzzn
             else:
                 print a, "is too low"
+                buzz.start(15)
+                time.sleep(.5)
+                buzz.stop()
     ans = raw_input("Play Again? [Y/N]:")
     if ans == "N":
         play = False
     elif ans == "Y":
         guess = True
 print "Game Exited"
-
-
+GPIO.cleanup()
